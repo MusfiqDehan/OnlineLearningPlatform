8b72dfd4acb82105ecc53b0d9981f61fa5319de8
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2024-07-17 00:34:34 +0600

    Setup allowed host and csrf_trusted_origins

--- OnlineLearningPlatform/settings.py
+++ OnlineLearningPlatform/settings.py
@@ -17,7 +17,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default="").split(',')
+
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', default="").split(',')
+
 
 DEBUG = os.getenv('DEBUG', default=True)
 
